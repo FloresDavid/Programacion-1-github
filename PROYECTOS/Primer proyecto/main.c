e8b1793ab91730c0565f3bf8acd179e0b43fadec
FloresDavid <36281512+FloresDavid@users.noreply.github.com>
2018-08-21 21:48:02 -0300

    Add files via upload

--- PROYECTOS/Primer proyecto/main.c
+++ PROYECTOS/Primer proyecto/main.c
@@ -1,38 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-//ingresar 3 numeros y saber el del medio
 
 int main()
 {
-
     int numeroUno;
     int numeroDos;
     int numeroTres;
     int numeroDelMedio;
+    int numeroMinimo;
+    int numeroMaximo;
+    int hayNumeroMedio = 0;
 
     printf("Ingrese el primer numero: ");
     scanf("%d", &numeroUno);
+    numeroMinimo = numeroUno;
+    numeroMaximo = numeroUno;
+
     printf("Ingrese el segundo numero: ");
     scanf("%d", &numeroDos);
+    if (numeroMinimo > numeroDos)
+    {
+        numeroMinimo = numeroDos;
+    }
+    else if (numeroMaximo < numeroDos)
+    {
+        numeroMaximo = numeroDos;
+    }
+
     printf("Ingrese el tercer numero: ");
     scanf("%d", &numeroTres);
+    if (numeroMinimo > numeroTres)
+    {
+        numeroMinimo = numeroTres;
+    }
+    else if (numeroMaximo < numeroTres)
+    {
+        numeroMaximo = numeroTres;
+    }
 
-    if((numeroUno > numeroDos && numeroUno < numeroTres) || (numeroUno < numeroDos && numeroUno > numeroTres))
+    if(numeroUno > numeroMinimo && numeroUno < numeroMaximo)
     {
         numeroDelMedio = numeroUno;
+        hayNumeroMedio = 1;
+    }
+    else if(numeroDos > numeroMinimo && numeroDos < numeroMaximo)
+    {
+        numeroDelMedio = numeroDos;
+        hayNumeroMedio = 1;
+    }
+    else if (numeroTres > numeroMinimo && numeroTres < numeroMaximo)
+    {
+        numeroDelMedio = numeroTres;
+        hayNumeroMedio = 1;
+    }
+
+    if (hayNumeroMedio == 0)
+    {
+        printf("No existe un numero del medio.");
     }
     else
     {
-        if((numeroDos > numeroUno && numeroDos < numeroTres) || (numeroDos < numeroUno && numeroDos > numeroTres))
-        {
-            numeroDelMedio = numeroDos;
-        }
-        else
-        {
-            numeroDelMedio = numeroTres;
-        }
+        printf("El numero del medio es el numero: %d", numeroDelMedio);
     }
-    printf("El numero del medio es el numero: %d", numeroDelMedio);
-
     return 0;
 }
